a37bc262b20a5f01ffaae15a7c6fe4141a06dfdf
FlorianMartinezRCO <f.martinez@recoveo.com>
2021-02-03 11:30:43 +0100

    Correctifs

--- templates/horaire/index.php
+++ templates/horaire/index.php
@@ -14,6 +14,7 @@
                 <th scope="col">Jour</th>
                 <th scope="col">Heure</th>
                 <th scope="col">Aperçu</th>
+                <th scope="col">Supprimer</th>
                 </tr>
             </thead>
             <tbody>
@@ -24,9 +25,9 @@
                     <td><?= $horaire->get_id_horaire() ?></td>
                     <td><?= $horaire->get_jour_horaire() ?></td>
                     <td><?= $horaire->get_heure_horaire() ?></td>
-                    <td><a href="?horaire=<?= $horaire->get_id_horaire() ?>"><button class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></button></a><td>
+                    <td><a href="?horaire=<?= $horaire->get_id_horaire() ?>"><button class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></button></a></td>
                     <td><button class="btn btn-danger btn-sm" id="supprimer_horaire"><i class="fas fa-trash-alt"></i></button>
-                    <a href="?supprimer_horaire=<?= $horaire->get_id_horaire() ?>" id="confirm_suppression" class="collapse"><button class="btn btn-danger btn-sm">Confirmer</button></a></td>
+                    <a href="?supprimer_horaire=<?= $horaire->get_id_horaire() ?>" id="confirm_suppression" style="display:none;"><button class="btn btn-danger btn-sm">Confirmer</button></a></td>
                 </tr>
                 <?php 
                     }
@@ -35,13 +36,13 @@
         </table>
     </div>
     <script>
-$(document).ready(function(){
-    $("#supprimer_horaire").click(function(){
-        
-        $('#confirm_suppression').show();
+    $(document).ready(function(){
+        $("#supprimer_horaire").click(function(){
+            
+            $('#confirm_suppression').show();
+        });
     });
-});
-</script>
+    </script>
 <?php } ?>
 
 <?php $contenu = ob_get_clean(); ?>
