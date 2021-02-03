17d6ec2c258cc3713dd7c1fffbdab06115e39a6c
FlorianMartinezRCO <f.martinez@recoveo.com>
2021-02-03 10:50:59 +0100

    Correctifs

--- templates/classe/show.php
+++ templates/classe/show.php
@@ -5,18 +5,10 @@
 <?php ob_start(); ?>
 
 <?php if(isset($classe)){ ?>
-<<<<<<< HEAD
-
-=======
->>>>>>> e71da0bd525b36790e7a6ade7b0e9a1de3307445
     <div class="container">
         <p>Id de la classe : <?= $classe->get_id_classe() ?></p>
         <p>Nom de la classe : <?= $classe->get_nom_classe() ?></p>
     </div>
-<<<<<<< HEAD
-
-=======
->>>>>>> e71da0bd525b36790e7a6ade7b0e9a1de3307445
 <?php } ?>
 
 <?php $contenu = ob_get_clean(); ?>
