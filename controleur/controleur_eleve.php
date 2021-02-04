8dc8e2ecaf4d306a9b1fc7c71e05974c5ed4f19c
Mathar <ma.arenam@gmail.com>
2021-02-04 14:49:38 +0100

    Modifications de ajouter eleve

--- controleur/controleur_eleve.php
+++ controleur/controleur_eleve.php
@@ -21,6 +21,17 @@
         $use_eleve->supprimer($_GET['supprimer_eleve']);
     } else if(isset($_GET['inscription_eleve'])){
         $use_eleve->inscription();
+    }  else if(isset($_GET['formulaire_ajout_eleve'])){
+        $responsable = $use_responsable->findAll();
+        $classe= $use_classe->findAll();
+        require_once($chemin . 'eleve/ajouter.php');
+    } else if(isset($_GET['ajouter_eleve'])){
+        $use_eleve->ajouter();
+    } else if(isset($_GET['formulaire_modifier_eleve'])){
+        $eleve = $use_eleve->findById($_GET['formulaire_modifier_eleve']);
+        require_once($chemin . 'eleve/modifier.php');
+    } else if(isset($_GET['modifier_eleve'])) {
+        $use_eleve->modifier($_GET['modifier_eleve']);
     }
 
 ?>
