7ecdabe26eb5c7cec9e287e27f4f83e2d8cda6b1
FlorianMartinezRCO <f.martinez@recoveo.com>
2021-02-04 16:30:35 +0100

    Fiche intervenant

--- templates/intervenant/show.php
+++ templates/intervenant/show.php
@@ -26,19 +26,51 @@
                     </div>
                 </div>
                 <div class="card mt-3 p-3">
-                    <h5 class="text-primary">Coordonnées</h5>
-                    <div class="card-body text-center">
+                    <div class="card-body">
+                        <h5 class="text-primary">Coordonnées</h5>
+                        <hr class="w-25">
                         <p>Adresse email : <?= $intervenant->get_mail_intervenant() ?> </p>
                         <p>Téléphone portable : <?= $intervenant->get_mobile_intervenant() ?> </p>
                         <p>Téléphone fixe : <?= $intervenant->get_fixe_intervenant() ?> </p>
+                        <h5 class="text-primary">Localisation</h5>
+                        <hr class="w-25">
+                        <p>Adresse : <?= $intervenant->get_adresse_intervenant() ?>, <?= $intervenant->get_cdp_intervenant() ?> (<?= $intervenant->get_ville_intervenant() ?>) </p>
                     </div>
                 </div>
             </div>
             <div class="col-7">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="text-primary">Pièces jointes</h5>
+                        <hr class="w-25">
+                        <p>Niveau : <?= $intervenant->get_niveau_intervenant() ?></p>
+                        <p>Spécialité : <?= $intervenant->get_specialite_intervenant() ?></p>
+                        <p>Profession : <?= $intervenant->get_profession_intervenant() ?></p>
+                        <p>Expérience : <?= $intervenant->get_experience_intervenant() ?></p>
+                        <p>Connaissance : <?= $intervenant->get_connaissance_intervenant() ?></p>
+
+                        <h5 class="text-primary">Pièces jointes</h5>
+                        <hr class="w-25">
+                        <?php if($intervenant->get_casier_intervenant() != null){ ?>
+                            <p>Casier judiciaire : <a href="documents/<?= $intervenant->get_casier_intervenant() ?>">Ouvrir le fichier</a></p>
+                        <?php } else { ?>
+                            <p>Casier judiciaire : <em>Aucun</em></p>
+                        <?php } ?>
+                        <?php if($intervenant->get_photo1_intervenant() != null){ ?>
+                            <p>Photo 1 : <a href="documents/<?= $intervenant->get_photo1_intervenant() ?>">Ouvrir le fichier</a></p>
+                        <?php } else { ?>
+                            <p>Photo 1 : <em>Aucune</em></p>
+                        <?php } ?>
+                        <?php if($intervenant->get_photo2_intervenant() != null){ ?>
+                            <p>Photo 2 : <a href="documents/<?= $intervenant->get_photo2_intervenant() ?>">Ouvrir le fichier</a></p>
+                        <?php } else { ?>
+                            <p>Photo 2 : <em>Aucune</em></p>
+                        <?php } ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
 
 <?php } ?>
 
